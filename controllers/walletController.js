890350e63c1623863d726e4bab1e5eb3d4c389f7
Rishabh Singh Sengar <sengartech@gmail.com>
2019-10-22 14:56:55 +0530

    added wallet setup and update controllers

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -15,19 +15,106 @@ let getAllWallets = (req, res) => {
   WalletModel.find({})
     .exec((err, result) => {
       if (err) {
-        console.log(`error occurred: ${err}`)
+        console.log(`error occurred: ${err}`);
         res.status(500).send({ error: true, message: `error occurred: ${err.message}`, data: null });
       } else {
-        console.log(`wallet list found.`)
+        console.log(`wallet list found.`);
         res.status(200).send({ error: false, message: `wallet list found.`, data: result });
       }
     })
 } // end of the getAllWallets function.
 
+/**
+ * function to setup wallet.
+ * required params: userId. 
+ * optional params: depositAmount, bonusAmount, winningAmount.
+ */
+let setupWallet = (req, res) => {
+  console.log('-- inside setupWallet function --');
+  if (!req.body.userId) {
+    console.log('please provide unique userId.');
+    res.status(400).send({ error: true, message: `please provide unique userId.`, data: null });
+  } else {
+    let depositAmount = Number(req.body.depositAmount) || 0;
+    let bonusAmount = Number(req.body.bonusAmount) || 0;
+    let winningAmount = Number(req.body.winningAmount) || 0;
+    let totalAmount = depositAmount + bonusAmount + winningAmount;
+
+    let newWallet = new WalletModel({
+      userId: req.body.userId,
+      depositAmount,
+      bonusAmount,
+      winningAmount,
+      totalAmount,
+    })
+
+    newWallet.save((err, result) => {
+      if (err) {
+        console.log(`error occurred: ${err}`);
+        res.status(500).send({ error: true, message: `error occurred: ${err.message}`, data: null });
+      } else {
+        console.log(`wallet created.`);
+        res.status(200).send({ error: false, message: `wallet created.`, data: result });
+      }
+    })
+  }
+} // end of the setupWallet function.
+
+/**
+ * function to update wallet.
+ * required params: userId. 
+ * optional params: depositAmount, bonusAmount, winningAmount.
+ */
+let updateWallet = (req, res) => {
+  console.log('-- inside updateWallet function --');
+  if (!req.body.userId) {
+    console.log('please provide unique userId.');
+    res.status(400).send({ error: true, message: `please provide unique userId.`, data: null });
+  } else if (!req.body.depositAmount && !req.body.bonusAmount && !req.body.winningAmount) {
+    console.log('no data provided to update.');
+    res.status(400).send({ error: true, message: `no data provided to update.`, data: null });
+  } else {
+    let findQuery = {
+      userId: req.body.userId
+    }
+
+    WalletModel.findOne(findQuery)
+      .exec((err, walletInfo) => {
+        if (err) {
+          console.log(`error occurred: ${err}`);
+          res.status(500).send({ error: true, message: `error occurred: ${err.message}`, data: null });
+        } else if (!walletInfo) {
+          console.log(`no wallet found for the given userId.`);
+          res.status(400).send({ error: true, message: `no wallet found for the given userId.`, data: null });
+        } else {
+          console.log(`wallet found updating...`);
+
+          let depositAmount = req.body.depositAmount ? Number(req.body.depositAmount) : walletInfo.depositAmount;
+          let bonusAmount = req.body.bonusAmount ? Number(req.body.bonusAmount) : walletInfo.bonusAmount;
+          let winningAmount = req.body.winningAmount ? Number(req.body.winningAmount) : walletInfo.winningAmount;
+          let totalAmount = depositAmount + bonusAmount + winningAmount;
+
+          walletInfo.depositAmount = depositAmount;
+          walletInfo.bonusAmount = bonusAmount;
+          walletInfo.winningAmount = winningAmount;
+          walletInfo.totalAmount = totalAmount;
+
+          walletInfo.modifiedOn = Date.now();
+
+          walletInfo.save((err, updatedWallet) => {
+            if (err) res.status(500).send({ error: true, message: `error occurred: ${err.message}`, data: null });
+            else res.status(200).send({ error: false, message: `wallet updated.`, data: updatedWallet });
+          })
+        } // end else.
+      })
+  }
+} // end of the setupWallet function.
 
 /**
  * exporting controller function.
  */
 module.exports = {
-  getAllWallets
+  getAllWallets,
+  setupWallet,
+  updateWallet
 }
--- routes/walletRoute.js
+++ routes/walletRoute.js
@@ -7,4 +7,13 @@ module.exports.routers = (app) => {
   // define all routes here.
 
   app.get('/wallet/get/all', walletController.getAllWallets);
+
+  // required params: userId.
+  // optional params: depositAmount, bonusAmount, winningAmount.
+  app.post('/wallet/setup', walletController.setupWallet);
+
+  // required params: userId.
+  // optional params: depositAmount, bonusAmount, winningAmount.
+  app.put('/wallet/update', walletController.updateWallet);
+
 } // end of router.
